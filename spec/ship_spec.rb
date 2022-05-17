86a6427360eaa260c258ee2438770dc2e33dbddf
Drew MacNicholas <dmacnicholas@gmail.com>
2022-05-17 15:52:22 -0500

    Add ship exists test

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,1 +1,10 @@
 require 'rspec'
+require './lib/ship'
+
+RSpec.describe Ship do
+  it 'ship exists' do
+    cruiser = Ship.new("Cruiser", 3)
+
+    expect(cruiser).to be_instance_of(Ship)
+  end
+end
