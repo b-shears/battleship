7d91328d11d15361cca66d4741c99b2a21f8797e
Bryan Shears <brshears@gmail.com>
2022-05-17 15:11:19 -0600

    Passed it has a name test.

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -11,9 +11,10 @@ RSpec.describe Ship do
     expect(@cruiser).to be_instance_of(Ship)
   end
 
-  xit 'has a name' do
-
-
+  it 'has a name' do
+    expect(@cruiser.name).to eq("Cruiser")
 
   end
+
+
 end
