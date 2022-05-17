fbd53f4609967b32b0b6935fb4dfdbdcbeb5239f
Bryan Shears <brshears@gmail.com>
2022-05-17 15:08:23 -0600

    Refactor Ship Test with before :each do method

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -2,9 +2,18 @@ require 'rspec'
 require './lib/ship'
 
 RSpec.describe Ship do
-  it 'ship exists' do
-    cruiser = Ship.new("Cruiser", 3)
+  #added in before each method. Use @cruiser when calling cruiser instance variable in test file.
+  before :each do
+    @cruiser = Ship.new("Cruiser", 3)
+  end
 
-    expect(cruiser).to be_instance_of(Ship)
+  it 'ship exists' do
+    expect(@cruiser).to be_instance_of(Ship)
+  end
+
+  xit 'has a name' do
+
+
+
   end
 end
