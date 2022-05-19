1cb1842ede99be5f283e5a519cfb2e23392fa29a
Lucas Colwell <colwellc37@gmail.com>
2022-05-18 20:17:15 -0600

    Write first two tests

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,1 +1,21 @@
 require 'rspec'
+require './lib/ship'
+require './lib/cell'
+
+RSpec.describe Cell do
+  describe 'Iteration 1' do
+    before :each do
+      @cell
+    end
+
+    it 'exists' do
+      expect(@cell).to be_a(Cell)
+    end
+
+    it 'has attributes' do
+      expect(@cell.coordinate).to eq("B4")
+    end
+
+  end
+
+end
