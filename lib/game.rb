317db4fe9be1f4fd523dab0dcf8e0e7bb8061657
Bryan Shears <brshears@gmail.com>
2022-05-23 22:52:19 -0600

    Work through player & computer ship placement

--- lib/game.rb
+++ lib/game.rb
@@ -5,6 +5,8 @@ class Game
   def initialize
     @player_board = Board.new
     @computer_board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
 
   end
 
@@ -13,7 +15,7 @@ class Game
     puts "Enter p to play. Enter q to quit."
 
     input = gets.chomp.downcase
-
+    input_2 = gets.chomp.upcase
       if input == "q"
         puts "You selected #{input}. Later Loser!"
 
@@ -22,6 +24,99 @@ class Game
       end
   end
 
+  def computer_ship_placement
+
+
+
+      cruiser_ship_cells = []
+
+       @cruiser.length.times do
+         x = @computer_board.cells.keys.sample()
+      if @computer_board.valid_coordinate?(x) == false
+        redo
+      else
+        cruiser_ship_cells << x
+
+      end
+    end
+
+
+      sub_ship_cells = []
+
+       @submarine.length.times do
+         x = @computer_board.cells.keys.sample()
+      if @computer_board.valid_coordinate?(x) == false
+        redo
+      else
+        sub_ship_cells  << x
+
+      end
+    end
+
+
+    cruiser_cells = []
+
+    @cruiser.length.times do
+      cruiser_ship_cells = @computer_board.cells.keys.sample()
+
+    if @computer_board.valid_placement?(@cruiser, cruiser_ship_cells) == true
+      @computer_board.place(@cruiser, cruiser_ship_cells)
+
+    else
+        cruiser_cells << cruiser_ship_cells
+    end
+  end
+  require "pry"; binding.pry
+
+    sub_cells = []
+    @submarine.length.times do
+      sub_ship_cells = @computer_board.cells.keys.sample()
+
+      if @computer_board.valid_placement?(@submarine, sub_ship_cells) == true
+        @computer_board.place(@submarine, sub_ship_cells)
+      end
+  end
+
+
+  end
+
+
+
+
+
+
+end
+
+        # cruiser_ship_cells = @computer_board.cells.keys.sample(3)
+
+
+
+        # if @computer_board.valid_placement?(@submarine, sub_ship_cells) == false
+        #
+        # else @computer_board.valid_placement?(@submarine, sub_ship_cells) == true
+        #   @computer_board.place(@submarine, sub_ship_cells)
+        #
+        #
+        # end
+
+
+
+
+
+  #
+  #   until @computer_board.valid_placement?(@submarine, sub_ship_cells) == true
+  #     redo
+  #
+  #  end
+  #
+  # end
+  #
+  # def sub_ship_cells
+  #
+  #   @computer_board.cells.keys.sample(2)
+  # end
+
+
   def player_ship_placement
 
     puts "I have laid out my ships on the grid."
@@ -29,7 +124,7 @@ class Game
     puts "The Cruiser is three units long and the Submarine is two units long."
     puts @player_board.render
     puts "Enter the squares for the Cruiser (3 spaces):"
-    puts ">" ["A1", "A2", "A3"]
+    puts ">" "A1", "A2", "A3"
 
         @coordinate_array = []
         coordinate_1 = gets.chomp.upcase
@@ -71,5 +166,4 @@ class Game
     #     @player_board.valid_coordinate?(coordinate_1) && @player_board.cells["#{coordinate_1}"].empty?
     #
     #   end
-  end
-end
+    end
